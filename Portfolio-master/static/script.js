69618a89d53994f25947ed85baad152b04d0343c
Matheus <theusma58@gmail.com>
2023-04-21 14:15:10 -0300

    animações prontas

--- Portfolio-master/static/script.js
+++ Portfolio-master/static/script.js
@@ -20,3 +20,23 @@ navItem.forEach(item => {
         }
     })
 })
+
+//Animar os itens que tiverem o atributo data-anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+
+    item.forEach(element => {
+        if (windowTop > element.offsetTop){
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate")
+        }
+    })
+
+}
+window.addEventListener("scroll", () =>{
+    animeScroll();
+})
